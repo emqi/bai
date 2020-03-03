55c8dc9354c4632a1d5b79d5c27d4407e37d682c
Marcin Skrzekut <marcin.skrzekut@gmail.com>
2020-03-03 15:45:54 +0100

    loops

--- 02-JSCoreConcepts/loop.js
+++ 02-JSCoreConcepts/loop.js
@@ -1,4 +1,18 @@
-console.log(2);
-console.log(6);
-console.log(10);
-console.log(14);
+const base = 2;
+
+// console.log(2);
+// console.log(6);
+// console.log(10);
+// console.log(14);
+
+let x = base;
+while (x <= 14) {
+    console.log(x);
+    x += 4;
+}
+
+console.log("\n");
+
+for (let x = base; x <=14; x+=4) {
+    console.log(x);
+}
