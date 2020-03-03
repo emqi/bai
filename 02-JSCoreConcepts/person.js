36b8ec6e14846402c2dc1cb208be88a020065750
Marcin Skrzekut <marcin.skrzekut@gmail.com>
2020-03-03 16:20:26 +0100

    person

---
+++ 02-JSCoreConcepts/person.js
@@ -0,0 +1,20 @@
+class Person {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    fullName() {
+        console.log(this.name + " " + this.surname.toUpperCase());
+    }
+    shortName() {
+        console.log(this.name.charAt(0).toUpperCase() + ". " + this.surname.charAt(0).toUpperCase() + ".");
+    }
+}
+
+jan = new Person('Jan', 'Nowak');
+ja = new Person('Johnny', 'Sins');
+
+jan.fullName();
+jan.shortName();
+ja.fullName();
+ja.shortName();
