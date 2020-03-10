dbd24fe6786c7eb54ef07fec729629265573a64f
Marcin Skrzekut <marcin.skrzekut@gmail.com>
2020-03-10 13:22:26 +0100

    formatting

--- 02-JSCoreConcepts/person.js
+++ 02-JSCoreConcepts/person.js
@@ -1,18 +1,23 @@
 class Person {
-    constructor(name, surname) {
-        this.name = name;
-        this.surname = surname;
-    }
-    fullName() {
-        console.log(this.name + " " + this.surname.toUpperCase());
-    }
-    shortName() {
-        console.log(this.name.charAt(0).toUpperCase() + ". " + this.surname.charAt(0).toUpperCase() + ".");
-    }
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+  fullName() {
+    console.log(this.name + " " + this.surname.toUpperCase());
+  }
+  shortName() {
+    console.log(
+      this.name.charAt(0).toUpperCase() +
+        ". " +
+        this.surname.charAt(0).toUpperCase() +
+        "."
+    );
+  }
 }
 
-jan = new Person('Jan', 'Nowak');
-ja = new Person('Johnny', 'Sins');
+jan = new Person("Jan", "Nowak");
+ja = new Person("Johnny", "Sins");
 
 jan.fullName();
 jan.shortName();
